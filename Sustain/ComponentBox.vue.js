a1e74f66b73464b95ec42eb7f05f2a7808aedeba
Akinsel1 <54415980+Akinsel1@users.noreply.github.com>
2020-05-21 15:26:43 -0600

    CarouselPanel
    Contents that function as slideshow

---
+++ Sustain/ComponentBox.vue.js
@@ -0,0 +1,22 @@
+var ComponentBox = {
+    name: "componentBox",
+    props:['contents'],
+    template: 
+    `<ul>
+    <li>
+    <div class="row">
+        <h1>Title</h1>
+    </div>
+    <div class="row">
+        <div class="col">
+            <p>paragraph</p>                   
+        </div>
+        <!-- Kens Burns -->
+        <div class="col">
+            <div class="img-wrapper">
+                <img src="https://picsum.photos/400">
+            </div>
+        </div>
+    </div>
+    </li></ul>`
+}
