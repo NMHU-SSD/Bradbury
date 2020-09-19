81750435d034256124d806b485eb8d7ea9e27743
Akinsel1 <54415980+Akinsel1@users.noreply.github.com>
2020-09-19 13:41:14 -0600

    Reusing Code
    Migrating the code

---
+++ app.js
@@ -0,0 +1,112 @@
+//register components
+Vue.component('building-future', buildingFuture)
+Vue.component('slideshow-component', slideshowComponent)
+Vue.component('tubie-overlay', tubieOverlay)
+Vue.component('modal-overlay', modalOverlay)
+
+
+//Vue
+var app = new Vue({
+    el: '#app',
+    data:{
+        title:"",
+        drive:"",
+        sustainable:"",
+        timeData:"",
+        t: null,
+        afk: null,
+        active:null,
+        firstData:true
+    },
+    mounted: function(){
+        this.GetData();
+        //this.resetTimer();
+    },
+    
+    methods:{
+        GetData: function(){
+            fetch("data/Data.json",{
+            	mode: 'cors'
+            })
+            .then(response => response.json())
+            .then(data =>{
+                this.timeData = data.timeData;
+                this.title = data.title;
+                this.drive = data.drive;
+                this.sustainable = data.sustainable;
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        },
+         //Timer modal functions
+        resetTimer:function(){
+            clearTimeout(this.t);
+            clearTimeout(this.afk);
+            if(this.active){
+                this.t = setTimeout(this.toAlert, this.timeData.timeout);
+                console.log("timer set");
+            }
+            else if(this.active==null){
+                this.active=true;
+            }
+            document.onmousedown = this.resetTimer;
+        },
+        toAlert:function(){
+            console.log("toAlert");
+            document.onmousedown = this.resetTimer;
+            this.$refs.timeModal.reset();
+            this.displayModal();
+            this.afk = setTimeout(this.toDefault, this.timeData.quitout);
+        },
+        toDefault:function(){
+            //this.active=false;
+            console.log("toDefault");
+            //this.$refs[this.computeId].reset();
+            //this.$refs[this.historyId].reset();
+        },
+        displayModal:function(){
+            $('#'+'modalTimer').modal();
+            this.$refs.timeModal.timer();
+            setTimeout(function(){
+                $('#'+'modalTimer').modal('hide')}, this.timeData.quitout);
+            document.onmousedown = this.resetTimer;
+        },
+        displayVideo:function(info){
+            if(this.active){
+                clearTimeout(this.t);
+                clearTimeout(this.afk);
+                this.active=false;
+            }
+            //this.stillActive=info.active;
+            //console.log(info);
+            this.$refs.modalVideo.geturl(info);
+            $('#modalVideo').modal();
+        },
+        //getter and setter videos
+        passVids:function(videos){
+            this.$refs[this.modalVideo].setVideos(videos);
+        },
+        getNextVid:function(index){
+            this.$refs[this.historyId].setNext(index);
+        },
+        getPrevVid:function(index){
+            this.$refs[this.historyId].setPrev(index);
+        },
+        //video modal popups
+        videoModalClose:function(){
+            this.active=true;
+            /*if(this.stillActive){
+                this.resetTimer();
+            }*/
+        },
+        switchData:function(){
+            if(this.firstData){
+                this.$refs.style1.reset();
+            }
+            else{this.$refs.style2.reset();}
+            this.firstData = !this.firstData;
+            console.log(this.firstData);
+        }
+    }
+})
