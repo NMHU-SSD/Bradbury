c685f5cfb2e8c3e97936d074e695df1fa33bb972
Rianne <9632612+littlepandabear@users.noreply.github.com>
2020-11-12 16:44:10 -0700

    fixed file
    filename

--- components/SlideshowComponent.vue.js
+++ components/SlideshowComponent.vue.js
@@ -1,41 +1,34 @@
 var slideshowComponent={
     name:"slideshow-component",
-    props:['images','speed','id','tubie','memo1','memo2','title1','title2'],
+    props:['images','speed','id','height','header','position'],
     data:function(){
         return{
             index:0,
             currentImg: null,
-            seconds: 0,
-            head:true
+            seconds: 0
         }
     },
     mounted:function(){
-        //console.log(this.images);
-        //this.initShow();
+        this.initShow();
+        //setTimeout(this.changeMult, this.speed);
     },
     methods:{
-        switchOut:function(){
-            this.head= !this.head;
-            this.$emit('switchout');
-        },
-        reset:function(){
-            this.head= true;
-        },
         slideshow:function(){
-            //console.log("slideshow");
             var fadeId = $('#'+this.id);
-            //$(fadeId).css('filter', 'brightness(0)');
+            $(fadeId).css('filter', 'brightness(0)');
             setTimeout(this.changeImg, 1000);
             setTimeout(function(){
-                $(fadeId).css('filter', 'brightness(50%)')}, 1000);
+                $(fadeId).css('filter', 'brightness(1)')}, 1000);
+            
             setTimeout(this.slideshow, this.speed);
         },
         changeImg:function(){
-            //console.log("change Image");
             var slideId = $('#'+this.id);
             this.currentImg = this.images[this.index].img;
             if(this.images[this.index].position){
                 $(slideId).css('object-position', this.images[this.index].position);
+            }else{
+                $(slideId).css('object-position', 'center');
             }
             if(this.index < this.images.length-1){
                 this.index++;
@@ -43,27 +36,27 @@ var slideshowComponent={
                 this.index=0;
             }
         },
+        changeMult:function(adjust){
+            for(x=0; x<3; i++){
+                var colId = $('#'+this.id+x);
+                this.currentImg = this.images[x].img
+                if(x>this.images.length-1){
+                    x=0;
+                }
+            }
+        },
         initShow:function(){
             this.seconds = Math.floor(this.speed / 1000);
-            console.log(this.images);
-            
-            var slideId = $('#'+this.id);
-            this.currentImg = this.images[this.index].img;
-            //console.log(this.speed);
-            if(this.images[this.index].position){
-                $(slideId).css('object-position', this.images[this.index].position);
-            }
-            this.index++;
-            //setTimeout(this.slideshow, this.speed);
+            console.log(this.currentImg);
             //:style="'animation: kenburns 20s infinite'"
         }
     },
     watch:{
         images:function(){
-            console.log("Watch triggered");
             var slideId = $('#'+this.id);
             if(this.images!=null){
                 this.currentImg = this.images[this.index].img;
+                console.log(this.id, this.currentImg);
                 if(this.images[this.index].position){
                     $(slideId).css('object-position', this.images[this.index].position);
                 }
@@ -73,19 +66,14 @@ var slideshowComponent={
         }
     },
     template:
-    `<div id="top-slide" class="slideshow-container whitetext">
-        <h1 v-show=head class="top yellow title-font shadow-text-big">Sustainable Supercomputing</h1>
-        <h1 v-show=!head class="top green title-font">Supercomputers Drive Sustainablilty</h1>
-        <img :id="id" :src="currentImg">
-        <div v-show=head class="center body-font shadow-text">{{ memo1 }}</div>
-        <div v-show=!head class="center body-font shadow-text">{{ memo2 }}</div>
-        <div class="tubie-container">
-            <tubie-overlay id="tubie-show" :display="tubie" spec="def"/>
+    `<div class="col slideshow-container">
+        <div :class="['splash-title-holder', (position!='left' ? 'splash-right' : '')]">
+            <div :class="['splash-title red', (position!='left' ? 'title-right' : 'title-left')]">
+                <img :src="header">
+            </div>
         </div>
-        <div class="banner red">
-            <div class="interest next-section" @click="switchOut()"></div>
-            <p v-show=head class="title-font">See how supercomputers drive a sustainable future</p>
-            <p v-show=!head class="title-font">Sustainable supercomputing</p>
+        <div v-show=true class="splash-background">
+            <img v-if=true :id="id" class="splash-img" :src="currentImg">
         </div>
     </div>`
 }
